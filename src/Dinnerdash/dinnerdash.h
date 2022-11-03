0a4745a71415b613aafdb1f714a64dcc4f35a426
Riandra4trend <16521427@std.stei.itb.ac.id>
2022-11-03 18:13:34 +0700

    feat: update dinnerdash

--- src/Dinnerdash/dinnerdash.h
+++ src/Dinnerdash/dinnerdash.h
@@ -1,11 +1,14 @@
 #ifndef DINNERDASH_H
 #define DINNERDASH_H
 #include <stdio.h>
-#include "queueDinner.h"
+#include "ADT/queueDinner.h"
 #include <stdlib.h>
 #include "../ADT/Boolean/boolean.h"
+#include "../ADT/Mesin/wordmachine.h"
 #include <time.h>
 
+extern char *query;
+extern char *arg;
 void faseAwal(int saldo, Queue Order, Queue Cook, Queue Rserve);
 void queueOrder(Queue *qOrder);
 void waktu(Queue *Order, Queue *Cook, Queue *RServe, char command[5]);
