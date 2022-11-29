74d8f8b042a2de960ab308f6e9c1b4e907e87533
Riandra4trend <18221077@std.stei.itb.ac.id>
2022-11-29 19:58:12 +0700

    feat: regen commits

---
+++ src/DinerDash/ADT_Local/queueDinner.h
@@ -0,0 +1,99 @@
+/* File : queue.h */
+/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi statik */
+
+#ifndef QUEUEDINNER_H
+#define QUEUEDINNER_H
+
+#include "../../ADT/Boolean/boolean.h"
+#include "../../ADT/Mesin/mesinkata.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define IDX_UNDEF -1
+#define CAPACITYF 7
+
+/* Definisi elemen dan address */
+typedef struct
+{
+        char *OrderId;
+        int durasi;
+        int ketahanan;
+        int harga;
+} Food;
+
+typedef struct
+{
+        Food buffer[CAPACITYF];
+        int idxHead;
+        int idxTail;
+} QueueF;
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika q adalah Queue, maka akses elemen : */
+#define IDX_HEAD(q) (q).idxHead
+#define IDX_TAIL(q) (q).idxTail
+#define HEAD(q) (q).buffer[(q).idxHead]
+#define TAIL(q) (q).buffer[(q).idxTail]
+
+/* *** Kreator *** */
+void CreateQueueF(QueueF *q);
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmptyF(QueueF q);
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFullF(QueueF q);
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+
+int lengthF(QueueF q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+
+/* *** Primitif Add/Delete *** */
+void enqueueF(QueueF *q, Food val);
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+
+void dequeueF(QueueF *q, Food *val);
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+        q mungkin kosong */
+
+int searchIdx(QueueF *q, char *id);
+
+/* *** Display Queue *** */
+void displayQueueOrder(QueueF q);
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
+
+void displayQueueCook(QueueF q);
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
+
+void displayQueueServe(QueueF q);
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
+
+#endif
---
+++ src/TowerofHanoi/StackofHanoi.c
@@ -0,0 +1,484 @@
+/* File : Stack.h */
+/* deklarasi Stackyang diimplementasi dengan tabel kontigu dan ukuran sama */
+/* TOP adalah alamat elemen puncak */
+/* Implementasi dalam bahasa C dengan alokasi statik */
+
+#include "StackofHanoi.h"
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateHanoi(StackHanoi *S)
+{
+    int i;
+    for (i = 0; i < MaxHanoi; i++)
+    {
+        (*S).T[i] = NilHanoi;
+    }
+    (*S).TOP = NilHanoi;
+}
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah StackHanoi S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri StackHanoi kosong : TOP berNilHanoiai NilHanoi */
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmptyHanoi(StackHanoi S)
+{
+    return (TopHanoi(S) == NilHanoi);
+}
+/* Mengirim true jika StackHanoi kosong: lihat definisi di atas */
+boolean IsFullHanoi(StackHanoi S)
+{
+    return (TopHanoi(S) == MaxHanoi - 1);
+}
+/* Mengirim true jika tabel penampung NilHanoiai elemen StackHanoi penuh */
+
+/* ************ Menambahkan sebuah elemen ke StackHanoi ************ */
+void PushHanoi(StackHanoi *S, infohanoi X)
+{
+    if (IsFullHanoi(*S))
+    {
+        printf("StackHanoi penuh\n");
+    }
+    else
+    {
+        TopHanoi(*S)++;
+        InfoTopHanoi(*S) = X;
+    }
+}
+/* Menambahkan X sebagai elemen StackHanoi S. */
+/* I.S. S mungkin kosong, tabel penampung elemen StackHanoi TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+
+/* ************ Menghapus sebuah elemen StackHanoi ************ */
+void PopHanoi(StackHanoi *S, infohanoi *X)
+{
+    if (IsEmptyHanoi(*S))
+    {
+        printf("StackHanoi kosong\n");
+    }
+    else
+    {
+        *X = InfoTopHanoi(*S);
+        InfoTopHanoi(*S) = NilHanoi;
+        TopHanoi(*S)--;
+    }
+}
+
+void displayStackHanoi(StackHanoi A, StackHanoi B, StackHanoi C, int n)
+{
+    int i;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        inttodisk(A.T[i], n);
+        inttodisk(B.T[i], n);
+        inttodisk(C.T[i], n);
+        printf("\n");
+    }
+    displaygaris(A, n);
+    displaygaris(B, n);
+    displaygaris(C, n);
+    printf("\n");
+    if (n > 2)
+    {
+        for (i = 0; i < n; i++)
+        {
+            printf(" ");
+        }
+        printf("A");
+        for (i = 0; i < n; i++)
+        {
+            printf(" ");
+        }
+        for (i = 0; i < n; i++)
+        {
+            printf(" ");
+        }
+        printf("B");
+        for (i = 0; i < n; i++)
+        {
+            printf(" ");
+        }
+        for (i = 0; i < n; i++)
+        {
+            printf(" ");
+        }
+        printf("C");
+        for (i = 0; i < n; i++)
+        {
+            printf(" ");
+        }
+    }
+    else
+    {
+        for (i = 0; i < 3; i++)
+        {
+            printf(" ");
+        }
+        printf("A");
+        for (i = 0; i < 3; i++)
+        {
+            printf(" ");
+        }
+        for (i = 0; i < 3; i++)
+        {
+            printf(" ");
+        }
+        printf("B");
+        for (i = 0; i < 3; i++)
+        {
+            printf(" ");
+        }
+        for (i = 0; i < 3; i++)
+        {
+            printf(" ");
+        }
+        printf("C");
+        for (i = 0; i < 3; i++)
+        {
+            printf(" ");
+        }
+    }
+    printf("\n\n");
+}
+
+void displaygaris(StackHanoi s, int n)
+{
+    int i;
+    int lenspace = 2 * n + 1;
+    if (n > 2)
+    {
+        if (IsEmptyHanoi(s))
+        {
+            for (i = 0; i < (lenspace - 3) / 2; i++)
+            {
+                printf(" ");
+            }
+            printf("---");
+            for (i = 0; i < (lenspace - 3) / 2; i++)
+            {
+                printf(" ");
+            }
+        }
+        else
+        {
+            for (i = 0; i < (lenspace - 7) / 2; i++)
+            {
+                printf(" ");
+            }
+            printf("-------");
+            for (i = 0; i < (lenspace - 7) / 2; i++)
+            {
+                printf(" ");
+            }
+        }
+    }
+    else
+    {
+        if (IsEmptyHanoi(s))
+        {
+            for (i = 0; i < 2; i++)
+            {
+                printf(" ");
+            }
+            printf("---");
+            for (i = 0; i < 2; i++)
+            {
+                printf(" ");
+            }
+        }
+        else
+        {
+            for (i = 0; i < 0; i++)
+            {
+                printf(" ");
+            }
+            printf("-------");
+            for (i = 0; i < 0; i++)
+            {
+                printf(" ");
+            }
+        }
+    }
+}
+
+void inttodisk(int x, int n)
+{
+    int i, j;
+    int lendisk = 2 * x - 1;
+    int lenspace = ((2 * n + 1) - lendisk) / 2;
+
+    if (n > 2)
+    {
+        if (x != NilHanoi)
+        {
+            for (i = 0; i < lenspace; i++)
+            {
+                printf(" ");
+            }
+            for (i = 0; i < lendisk; i++)
+            {
+                printf("*");
+            }
+            for (i = 0; i < lenspace; i++)
+            {
+                printf(" ");
+            }
+        }
+        else
+        {
+            for (i = 0; i < n; i++)
+            {
+                printf(" ");
+            }
+            printf("|");
+            for (i = 0; i < n; i++)
+            {
+                printf(" ");
+            }
+        }
+    }
+    else if (n == 2)
+    {
+        if (x != NilHanoi)
+        {
+            for (i = 0; i < (7 - lendisk) / 2; i++)
+            {
+                printf(" ");
+            }
+            for (i = 0; i < lendisk; i++)
+            {
+                printf("*");
+            }
+            for (i = 0; i < (7 - lendisk) / 2; i++)
+            {
+                printf(" ");
+            }
+        }
+        else
+        {
+            for (i = 0; i < 3; i++)
+            {
+                printf(" ");
+            }
+            printf("|");
+            for (i = 0; i < 3; i++)
+            {
+                printf(" ");
+            }
+        }
+    }
+    else
+    {
+        if (x != NilHanoi)
+        {
+            for (i = 0; i < (7 - lendisk) / 2; i++)
+            {
+                printf(" ");
+            }
+            for (i = 0; i < lendisk; i++)
+            {
+                printf("*");
+            }
+            for (i = 0; i < (7 - lendisk) / 2; i++)
+            {
+                printf(" ");
+            }
+        }
+        else
+        {
+            for (i = 0; i < 3; i++)
+            {
+                printf(" ");
+            }
+            printf("|");
+            for (i = 0; i < 3; i++)
+            {
+                printf(" ");
+            }
+        }
+    }
+}
+
+void move(char *asal, char *tujuan, StackHanoi *A, StackHanoi *B, StackHanoi *C, int n)
+{
+    infohanoi x;
+    if (*asal == 'A' && !IsEmptyHanoi(*A))
+    {
+        PopHanoi(A, &x);
+        if (*tujuan == 'B')
+        {
+            PushHanoi(B, x);
+            printf("Memindahkan piringan ke B...\n\n\n");
+        }
+        else if (*tujuan == 'C')
+        {
+            PushHanoi(C, x);
+            printf("Memindahkan piringan ke C...\n\n\n");
+        }
+    }
+    else if (*asal == 'B' && !IsEmptyHanoi(*B))
+    {
+        PopHanoi(B, &x);
+        if (*tujuan == 'A')
+        {
+            PushHanoi(A, x);
+            printf("Memindahkan piringan ke A...\n\n\n");
+        }
+        else if (*tujuan == 'C')
+        {
+            PushHanoi(C, x);
+            printf("Memindahkan piringan ke C...\n\n\n");
+        }
+    }
+    else if (*asal == 'C' && !IsEmptyHanoi(*C))
+    {
+        PopHanoi(C, &x);
+        if (*tujuan == 'A')
+        {
+            PushHanoi(A, x);
+            printf("Memindahkan piringan ke A...\n\n\n");
+        }
+        else if (*tujuan == 'B')
+        {
+            PushHanoi(B, x);
+            printf("Memindahkan piringan ke B...\n\n\n");
+        }
+    }
+    else
+    {
+        printf("Tidak bisa dipindahkan\n");
+    }
+    displayStackHanoi(*A, *B, *C, n);
+}
+
+boolean isvalid(char *asal, char *tujuan, StackHanoi *A, StackHanoi *B, StackHanoi *C)
+{
+    if (*asal != *tujuan)
+    {
+        if (*asal == 'A' && *tujuan == 'B')
+        {
+            if (IsEmptyHanoi(*A) || (!IsEmptyHanoi(*B) && InfoTopHanoi(*A) > InfoTopHanoi(*B)))
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+        else if (*asal == 'A' && *tujuan == 'C')
+        {
+            if (IsEmptyHanoi(*A) || (!IsEmptyHanoi(*C) && InfoTopHanoi(*A) > InfoTopHanoi(*C)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (*asal == 'B' && *tujuan == 'C')
+        {
+            if (IsEmptyHanoi(*B) || (!IsEmptyHanoi(*C) && InfoTopHanoi(*B) > InfoTopHanoi(*C)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (*asal == 'B' && *tujuan == 'A')
+        {
+            if (IsEmptyHanoi(*B) || (!IsEmptyHanoi(*A) && InfoTopHanoi(*B) > InfoTopHanoi(*A)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (*asal == 'C' && *tujuan == 'A')
+        {
+            if (IsEmptyHanoi(*C) || (!IsEmptyHanoi(*A) && InfoTopHanoi(*C) > InfoTopHanoi(*A)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (*asal == 'C' && *tujuan == 'B')
+        {
+            if (IsEmptyHanoi(*C) || (!IsEmptyHanoi(*B) && InfoTopHanoi(*C) > InfoTopHanoi(*B)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int lengthStackHanoi(StackHanoi S)
+{
+    return TopHanoi(S) + 1;
+}
+
+boolean isnumber(char *x)
+{
+    int i;
+    for (i = 0; i < len(x); i++)
+    {
+        if (x[i] < '0' || x[i] > '9')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int len(char *x)
+{
+    int i = 0;
+    while (x[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+int strtoint(char *x)
+{
+    int i;
+    int hasil = 0;
+    for (i = 0; i < len(x); i++)
+    {
+        hasil = hasil * 10 + (x[i] - '0');
+    }
+    return hasil;
+}
+
+int currenkatalength(char *x)
+{
+    int i = 0;
+    while (x[i] != ' ' && x[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
